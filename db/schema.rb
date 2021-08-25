0e1bf54fbe69dc1779ad69ade233b1eae5fe549c
Andrew Massey <acmassey@ncsu.edu>
2021-08-24 20:28:16 -0400

    Schema change to match new foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_232034) do
+ActiveRecord::Schema.define(version: 2021_08_25_000140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_08_24_232034) do
     t.integer "calories"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "restaurant_id"
+    t.index ["restaurant_id"], name: "index_menu_items_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_08_24_232034) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "menu_items", "restaurants"
 end
