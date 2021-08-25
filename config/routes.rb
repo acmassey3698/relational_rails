ac82ba5a1648f5bdf671053ffe2c389dbdbca3ef
Ted S <tstaros23@gmail.com>
2021-08-24 19:39:49 -0600

    Add drinks table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/menu_items', to: 'menu_items#index'
-
+  get '/drinks', to: 'drinks#index'
 end
