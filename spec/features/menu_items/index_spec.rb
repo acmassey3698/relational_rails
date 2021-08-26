f3e7d2b4002bb2167f4ce7d854269f9c633e6535
Andrew Massey <acmassey@ncsu.edu>
2021-08-26 15:56:45 -0400

    Updated view and test file to include all attributes

--- spec/features/menu_items/index_spec.rb
+++ spec/features/menu_items/index_spec.rb
@@ -36,4 +36,52 @@ RSpec.describe 'menu items index' do
     expect(page).to have_content(item_1.calories)
     expect(page).to have_content(item_2.calories)
   end
+
+  it "displays menu item ID" do
+    restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
+
+    item_1 = MenuItem.create!(name: "Big Mac", vegetarian: false, calories: 1000, restaurant_id: restaurant.id)
+    item_2 = MenuItem.create!(name: "Chicken Sandwich", vegetarian: false, calories: 400, restaurant_id: restaurant.id)
+
+    visit "/menu_items"
+
+    expect(page).to have_content(item_1.id)
+    expect(page).to have_content(item_2.id)
+  end
+
+  it "displays menu item's restaurant ID" do
+    restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
+
+    item_1 = MenuItem.create!(name: "Big Mac", vegetarian: false, calories: 1000, restaurant_id: restaurant.id)
+    item_2 = MenuItem.create!(name: "Chicken Sandwich", vegetarian: false, calories: 400, restaurant_id: restaurant.id)
+
+    visit "/menu_items"
+
+    expect(page).to have_content(item_1.restaurant_id)
+    expect(page).to have_content(item_2.restaurant_id)
+  end
+
+  it "displays menu item created at" do
+    restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
+
+    item_1 = MenuItem.create!(name: "Big Mac", vegetarian: false, calories: 1000, restaurant_id: restaurant.id)
+    item_2 = MenuItem.create!(name: "Chicken Sandwich", vegetarian: false, calories: 400, restaurant_id: restaurant.id)
+
+    visit "/menu_items"
+
+    expect(page).to have_content(item_1.created_at)
+    expect(page).to have_content(item_2.created_at)
+  end
+
+  it "displays menu item updated at" do
+    restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
+
+    item_1 = MenuItem.create!(name: "Big Mac", vegetarian: false, calories: 1000, restaurant_id: restaurant.id)
+    item_2 = MenuItem.create!(name: "Chicken Sandwich", vegetarian: false, calories: 400, restaurant_id: restaurant.id)
+
+    visit "/menu_items"
+
+    expect(page).to have_content(item_1.updated_at)
+    expect(page).to have_content(item_2.updated_at)
+  end
 end
