619110265eec883b4767a4627eb7584927cc35ba
Andrew Massey <acmassey@ncsu.edu>
2021-08-27 19:11:49 -0400

    Remove testing for attributes that are no longer displayed

--- spec/features/menu_items/show_spec.rb
+++ spec/features/menu_items/show_spec.rb
@@ -33,48 +33,4 @@ RSpec.describe 'menu item show' do
 
     expect(page).to have_content(item_1.calories)
   end
-
-  it "displays item created at" do
-    restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
-    other_restaurant = Restaurant.create!(name: "Taco Bell", delivery: false, yelp_rating: 4)
-    item_1 = MenuItem.create!(name: "Big Mac", vegetarian: false, calories: 1000, restaurant_id: restaurant.id)
-    item_2 = MenuItem.create!(name: "Bean Burrito", vegetarian: true, calories: 300, restaurant_id: other_restaurant.id)
-
-    visit "/menu_items/#{item_1.id}"
-
-    expect(page).to have_content(item_1.created_at)
-  end
-
-  it "displays item updated at" do
-    restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
-    other_restaurant = Restaurant.create!(name: "Taco Bell", delivery: false, yelp_rating: 4)
-    item_1 = MenuItem.create!(name: "Big Mac", vegetarian: false, calories: 1000, restaurant_id: restaurant.id)
-    item_2 = MenuItem.create!(name: "Bean Burrito", vegetarian: true, calories: 300, restaurant_id: other_restaurant.id)
-
-    visit "/menu_items/#{item_1.id}"
-
-    expect(page).to have_content(item_1.updated_at)
-  end
-
-  it "displays item ID" do
-    restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
-    other_restaurant = Restaurant.create!(name: "Taco Bell", delivery: false, yelp_rating: 4)
-    item_1 = MenuItem.create!(name: "Big Mac", vegetarian: false, calories: 1000, restaurant_id: restaurant.id)
-    item_2 = MenuItem.create!(name: "Bean Burrito", vegetarian: true, calories: 300, restaurant_id: other_restaurant.id)
-
-    visit "/menu_items/#{item_1.id}"
-
-    expect(page).to have_content(item_1.id)
-  end
-
-  it "displays item restaurant ID" do
-    restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
-    other_restaurant = Restaurant.create!(name: "Taco Bell", delivery: false, yelp_rating: 4)
-    item_1 = MenuItem.create!(name: "Big Mac", vegetarian: false, calories: 1000, restaurant_id: restaurant.id)
-    item_2 = MenuItem.create!(name: "Bean Burrito", vegetarian: true, calories: 300, restaurant_id: other_restaurant.id)
-
-    visit "/menu_items/#{item_1.id}"
-
-    expect(page).to have_content(item_1.restaurant_id)
-  end
 end
--- spec/features/restaurants/show_spec.rb
+++ spec/features/restaurants/show_spec.rb
@@ -30,33 +30,4 @@ RSpec.describe 'restaurants show' do
     expect(page).to have_content(restaurant_1.delivery)
     expect(page).to_not have_content(restaurant_2.delivery)
   end
-
-  it "displays restaurant id" do
-    restaurant_1 = Restaurant.create!(name: 'McDonalds', delivery: false, yelp_rating: 4,)
-    restaurant_2 = Restaurant.create!(name: 'Taco Bell', delivery: true, yelp_rating: 5,)
-
-    visit "/restaurants/#{restaurant_1.id}"
-
-    expect(page).to have_content(restaurant_1.id)
-    expect(page).to_not have_content(restaurant_2.id)
-  end
-
-  it "displays restaurant created at" do
-    restaurant_1 = Restaurant.create!(name: 'McDonalds', delivery: false, yelp_rating: 4,)
-    restaurant_2 = Restaurant.create!(name: 'Taco Bell', delivery: true, yelp_rating: 5,)
-
-    visit "/restaurants/#{restaurant_1.id}"
-
-    expect(page).to have_content(restaurant_1.created_at)
-  end
-
-  it "displays restaurant updated at" do
-    restaurant_1 = Restaurant.create!(name: 'McDonalds', delivery: false, yelp_rating: 4,)
-    restaurant_2 = Restaurant.create!(name: 'Taco Bell', delivery: false, yelp_rating: 5,)
-
-    visit "/restaurants/#{restaurant_1.id}"
-
-    expect(page).to have_content(restaurant_1.updated_at)
-  end
-
 end
