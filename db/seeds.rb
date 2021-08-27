27e7ce542c7747fd04d4fdd5e04c77aa3a7cbd9d
Ted S <tstaros23@gmail.com>
2021-08-26 18:42:48 -0600

    Update seed and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@pioneer = Bar.create!(name: 'Pioneer', has_food: true, tabs: 2)
+@crimson_and_gold = Bar.create!(name: 'Crimson and Gold', has_food: true, tabs: 1)
+@daves = Bar.create!(name: 'Daves', has_food: false, tabs: 10)
+@rum = @pioneer.drinks.create!(name: 'Rum and Coke', contains_alcohol: true, drink_rating: 5)
+@water = @pioneer.drinks.create!(name: 'Water', contains_alcohol: false, drink_rating: 4)
+@coke = @daves.drinks.create!(name: 'Coke', contains_alcohol: false, drink_rating: 3)
+@gin = @daves.drinks.create!(name: 'Gin', contains_alcohol: true, drink_rating: 2)
+@vodka = @crimson_and_gold.drinks.create!(name: 'Vodka', contains_alcohol: true, drink_rating: 5)
+@whiskey = @crimson_and_gold.drinks.create!(name: 'Whiskey', contains_alcohol: true, drink_rating: 4)
