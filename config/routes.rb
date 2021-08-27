dbe3c5a6086fd325a2d77188f675010c56625ba4
Andrew Massey <acmassey@ncsu.edu>
2021-08-26 20:44:13 -0400

    New routing for creating restaruants

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/restaurants', to: 'restaurants#index'
+  get "/restaurants/new", to: 'restaurants#new'
+  post '/restaurants', to: 'restaurants#create'
   get '/menu_items', to: 'menu_items#index'
+
   get '/restaurants/:id', to: 'restaurants#show'
   get '/menu_items/:id', to: 'menu_items#show'
   get '/drinks', to: 'drinks#index'
@@ -9,5 +12,6 @@ Rails.application.routes.draw do
 
   get '/bars', to: 'bars#index'
   get '/restaurants/:id/menu_items', to: 'restaurants#restaurant_menu_items'
+
 
 end
