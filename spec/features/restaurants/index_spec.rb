bacd1f355a31cec677ed8bbb77d0a65aba3ace9d
Andrew Massey <acmassey@ncsu.edu>
2021-08-29 21:01:58 -0400

    Added testing for link paths

--- spec/features/restaurants/index_spec.rb
+++ spec/features/restaurants/index_spec.rb
@@ -11,6 +11,17 @@ RSpec.describe 'restaurants index' do
     expect(page).to have_content(restaurant_2.name)
   end
 
+  it "links to the restaurant show page when you click on the restaurant name" do
+    restaurant_1 = Restaurant.create!(name: 'McDonalds', delivery: false, yelp_rating: 4,)
+    restaurant_2 = Restaurant.create!(name: 'Taco Bell', delivery: false, yelp_rating: 4,)
+
+    visit "/restaurants"
+
+    click_link('McDonalds')
+
+    expect(current_path).to eq("/restaurants/#{restaurant_1.id}")
+  end
+
   it "displays the nav links at the top of the page" do
     restaurant_1 = Restaurant.create!(name: 'McDonalds', delivery: false, yelp_rating: 4,)
     restaurant_2 = Restaurant.create!(name: 'Taco Bell', delivery: false, yelp_rating: 4,)
