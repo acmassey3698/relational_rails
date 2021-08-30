edd9d0353015c6c7c48fb944af00cc11cd6b60b1
Andrew Massey <acmassey@ncsu.edu>
2021-08-29 21:01:44 -0400

    Added testing for link paths

--- spec/features/menu_items/index_spec.rb
+++ spec/features/menu_items/index_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'menu items index' do
+
   it "displays menu item names" do
     restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
 
@@ -11,6 +12,19 @@ RSpec.describe 'menu items index' do
 
     expect(page).to have_content(item_1.name)
     expect(page).to have_content(item_2.name)
+  end
+
+  it 'links to the menu item show page when you click on the items name' do
+    restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
+
+    item_1 = MenuItem.create!(name: "Big Mac", vegetarian: false, calories: 1000, restaurant_id: restaurant.id)
+    item_2 = MenuItem.create!(name: "Chicken Sandwich", vegetarian: false, calories: 400, restaurant_id: restaurant.id)
+
+    visit "/menu_items"
+
+    click_on("Big Mac")
+
+    expect(current_path).to eq("/menu_items/#{item_1.id}")
   end
 
   it "displays if menu item vegetarian" do
