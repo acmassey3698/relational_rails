abedc726c612bcf229a524bb86a818fd3188ff19
Andrew Massey <acmassey@ncsu.edu>
2021-08-30 15:22:45 -0400

    Create seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Bar.destroy_all
+Drink.destroy_all
+Restaurant.destroy_all
+MenuItem.destroy_all
+
 @pioneer = Bar.create!(name: 'Pioneer', has_food: true, tabs: 2)
 @crimson_and_gold = Bar.create!(name: 'Crimson and Gold', has_food: true, tabs: 1)
 @daves = Bar.create!(name: 'Daves', has_food: false, tabs: 10)
@@ -15,3 +20,17 @@
 @gin = @daves.drinks.create!(name: 'Gin', contains_alcohol: true, drink_rating: 2)
 @vodka = @crimson_and_gold.drinks.create!(name: 'Vodka', contains_alcohol: true, drink_rating: 5)
 @whiskey = @crimson_and_gold.drinks.create!(name: 'Whiskey', contains_alcohol: true, drink_rating: 4)
+
+@mcdonalds = Restaurant.create!(name: 'McDonalds', delivery: true, yelp_rating: 3)
+@taco_bell = Restaurant.create!(name: 'Taco Bell', delivery: true, yelp_rating: 5)
+@olive_garden = Restaurant.create!(name: 'Olive Garden', delivery: false, yelp_rating: 3)
+
+@big_mac = @mcdonalds.menu_items.create!(name: "Big Mac", calories: 1000, vegetarian: false)
+@mcflurry = @mcdonalds.menu_items.create!(name: "McFlurry", calories: 400, vegetarian: true)
+@apple_pie = @mcdonalds.menu_items.create!(name: "Apple Pie", calories: 600, vegetarian: true)
+@crunchwrap = @taco_bell.menu_items.create!(name: "Crunchwrap Supreme", calories: 500, vegetarian: false )
+@bean_burrito = @taco_bell.menu_items.create!(name: "Bean Burrito", calories: 300, vegetarian: true )
+@baja_blast = @taco_bell.menu_items.create!(name: "Baja Blast Freeze", calories: 280, vegetarian: true )
+@carbonara = @olive_garden.menu_items.create!(name: "Carbonara", calories: 1200, vegetarian: false)
+@veggie_lasagna = @olive_garden.menu_items.create!(name: "Vegetable Lasagna", calories: 700, vegetarian: true)
+@breadsticks = @olive_garden.menu_items.create!(name: "Breadsticks", calories:300, vegetarian: true)
