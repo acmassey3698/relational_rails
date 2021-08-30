90e6e5a3f2aaa5b0020a9eee34de00d79f2e00c5
Andrew Massey <acmassey@ncsu.edu>
2021-08-30 19:02:00 -0400

    Create spec for posting of new menu items

--- spec/features/restaurants/menu_items/new_spec.rb
+++ spec/features/restaurants/menu_items/new_spec.rb
@@ -18,8 +18,26 @@ RSpec.describe 'Restaurant menu item creation' do
     item_2 = MenuItem.create!(name: "Chicken Sandwich", vegetarian: false, calories: 400, restaurant_id: restaurant.id)
 
     visit "/restaurants/#{restaurant.id}/menu_items"
-    click_link 'Add Menu Item'
+    click_on 'Add Menu Item'
 
     expect(current_path).to eq("/restaurants/#{restaurant.id}/menu_items/new")
+  end
+
+  it 'creates a new menu item for that restaurant' do
+    restaurant = Restaurant.create!(name: "Mcdonalds", delivery: false, yelp_rating: 5)
+
+    item_1 = MenuItem.create!(name: "Big Mac", vegetarian: false, calories: 1000, restaurant_id: restaurant.id)
+    item_2 = MenuItem.create!(name: "Chicken Sandwich", vegetarian: false, calories: 400, restaurant_id: restaurant.id)
+
+    visit "/restaurants/#{restaurant.id}/menu_items"
+    click_link 'Add Menu Item'
+
+    fill_in 'name', with: 'McFlurry'
+    fill_in('calories', with: 400)
+    choose(option: 'true')
+    click_button 'Create Menu Item'
+
+    expect(current_path).to eq("/restaurants/#{restaurant.id}/menu_items")
+    expect(page).to have_content('McFlurry')
   end
 end
