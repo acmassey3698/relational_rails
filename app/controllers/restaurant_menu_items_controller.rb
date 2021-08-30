0b2225469f845b684d2073c9b5c8b89829611555
Andrew Massey <acmassey@ncsu.edu>
2021-08-30 19:02:42 -0400

    Update controller with create action

--- app/controllers/restaurant_menu_items_controller.rb
+++ app/controllers/restaurant_menu_items_controller.rb
@@ -4,6 +4,17 @@ class RestaurantMenuItemsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.find(params[:id])
+  end
 
+  def create
+    restaurant = Restaurant.find(params[:id])
+    restaurant.menu_items.create(menu_items_params)
+    redirect_to "/restaurants/#{restaurant.id}/menu_items"
+  end
+
+private
+  def menu_items_params
+    params.permit(:name, :calories, :vegetarian)
   end
 end
